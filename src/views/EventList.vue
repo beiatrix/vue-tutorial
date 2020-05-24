b9a993dd511b7edc4ba654ff876e8812e8857187
beiatrix <bpedrasa@gmail.com>
2020-05-24 15:20:05 -0700

    Add user module

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,23 +1,20 @@
 <template>
   <div>
-    <h1>Events Listing</h1>
+    <h1>Events for {{ user.user.name }}</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event" />
     <template v-if="page != 1">
       <router-link
         :to="{ name: 'event-list', query: { page: page - 1 } }"
         rel="prev"
-      >
-        Prev Page
-      </router-link>
-      |
+        >Prev Page</router-link
+      >|
     </template>
     <template v-if="this.eventsTotal > this.page * 5">
       <router-link
         :to="{ name: 'event-list', query: { page: page + 1 } }"
         rel="next"
+        >Next Page</router-link
       >
-        Next Page
-      </router-link>
     </template>
   </div>
 </template>
@@ -40,7 +37,7 @@ export default {
     page() {
       return parseInt(this.$route.query.page) || 1;
     },
-    ...mapState(['events', 'eventsTotal']),
+    ...mapState(['events', 'eventsTotal', 'user']),
   },
 };
 </script>
