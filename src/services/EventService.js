33ce5cb817ff3f5918a768df027fa322144a4992
beiatrix <bpedrasa@gmail.com>
2020-06-08 16:19:27 -0700

    Progress bar - use in-route component guards

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import NProgress from 'nprogress';
 
 const apiClient = axios.create({
   baseURL: 'http://localhost:2020',
@@ -8,17 +7,6 @@ const apiClient = axios.create({
     Accept: 'application/json',
     'Content-Type': 'application/json',
   },
-});
-
-// axios interceptors - like middleware
-apiClient.interceptors.request.use(config => {
-  NProgress.start();
-  return config;
-});
-
-apiClient.interceptors.response.use(response => {
-  NProgress.done();
-  return response;
 });
 
 export default {
